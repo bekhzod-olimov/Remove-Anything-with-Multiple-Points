1ebc3b58c9706d36b04dd60025cc644cc612ef0a
bekhzod-olimov <bekhzod.olimov@gmail.com>
2024-11-13 09:07:50 +0900

    add replacer to demo

--- demo.py
+++ demo.py
@@ -15,71 +15,82 @@ def run(args):
 
     assert args.lang in ["en", "ko"], "Please choose English (en) or Korean (ko) language."
 
-    if   args.lang == "en": type1, type2, label, placeholder = "my_image", "existing_image", "Please choose a demo type", "Please click to choose"
-    elif args.lang == "ko": type1, type2, label, placeholder = "본인 이미지", "리스트에 있는 이미지", "데모 종류를 선택해주세요", "선택을 위해 클릭해주세요"
+    if   args.lang == "en": task1, task2, label, placeholder, final_header = "replace", "inpaint", "Please choose a task:", "Please click to choose", "Please rerun streamlit script from the terminal!"
+    elif args.lang == "ko": task1, task2, label, placeholder, final_header = "대체", "인페인팅", "작업을 선택해 주세요:", "선택을 위해 클릭해주세요", "터미널에서 streamlit 스크립트를 다시 실행해 주세요!"
 
-    input_points, demo_types = None, [type1, type2]
+    task_options = [task1, task2]
     
-    type_name = choose(option = demo_types, label = label, placeholder = placeholder)
+    task_name = choose(option = task_options, label = label, placeholder = placeholder)
 
-    if type_name in ["my_image", "본인 이미지"]:
+    if task_name in ["inpaint", "인페인팅"]:
 
-        if   args.lang == "en": im_path_lbl, input_points_lbl,  input_labels_lbl = "Please type image path:", "Please type input points:", "Please type input labels:"
-        elif args.lang == "ko": im_path_lbl, input_points_lbl,  input_labels_lbl = "이미지 경로를 입력해 주세요:", "입력 포인트를 입력해 주세요:", "입력 레이블을 입력해 주세요:"
+        if   args.lang == "en": type1, type2, label = "my_image", "existing_image", "Please choose a demo type:"
+        elif args.lang == "ko": type1, type2, label = "본인 이미지", "리스트에 있는 이미지", "데모 종류를 선택해주세요:"
 
-        im_path       = st.text_input(label = im_path_lbl,      value = None)
-        input_points  = st.text_input(label = input_points_lbl, value = None)
-        input_labels  = st.text_input(label = input_labels_lbl, value = None)
+        input_points, demo_types = None, [type1, type2]
+        
+        type_name = choose(option = demo_types, label = label, placeholder = placeholder)
 
-        if (not input_labels is None) and (not input_points is None): input_points, input_labels = get_coords(input_points), get_labels(input_labels)      
+        if type_name in ["my_image", "본인 이미지"]:
 
-    elif type_name in ["existing_image", "리스트에 있는 이미지"]:
+            if   args.lang == "en": im_path_lbl, input_points_lbl,  input_labels_lbl = "Please type image path:", "Please type input points:", "Please type input labels:"
+            elif args.lang == "ko": im_path_lbl, input_points_lbl,  input_labels_lbl = "이미지 경로를 입력해 주세요:", "입력 포인트를 입력해 주세요:", "입력 레이블을 입력해 주세요:"
 
-        if   args.lang == "en": st.header("Please upload an image or choose from the list:") 
-        elif args.lang == "ko": st.header("이미지를 업로드하거나 이미지를 선택해주세요:") 
+            im_path       = st.text_input(label = im_path_lbl,      value = None)
+            input_points  = st.text_input(label = input_points_lbl, value = None)
+            input_labels  = st.text_input(label = input_labels_lbl, value = None)
 
-        input_points, input_labels = args.point_coords, args.point_labels    
+            if (not input_labels is None) and (not input_points is None): input_points, input_labels = get_coords(input_points), get_labels(input_labels)      
 
-        get_im  = st.file_uploader('1', label_visibility='collapsed')
-        im_path = None
-        ims_lst, image_captions = get_ims_captions(path=args.root, n_ims=7)   
+        elif type_name in ["existing_image", "리스트에 있는 이미지"]:
 
-        # Use the selected or uploaded image to get points and labels
-        if get_im: input_points, input_labels = get_clicked_point(get_im)          
-            
-        elif (get_im == None) and (im_path == None): 
-            
-            select_label = "Images List" if args.lang == "en" else "이미지 목록"
-            choice_label = "Available Images" if args.lang == "en" else "선택 가능한 이미지 목록"
-            placeholder  = "Please click to choose" if args.lang == "en" else "선택을 위해 클릭해주세요"
-            image_select(label=select_label, images=ims_lst, captions=image_captions)
-            choice = choose(option = image_captions, label = choice_label, placeholder = placeholder)
-            
-            if   args.lang == "en": st.header("Please upload an image or choose from the list!") 
-            elif args.lang == "ko": st.header("이미지를 선택하거나 업로드 해주세요!") 
-            
-            if choice:  
+            if   args.lang == "en": st.header("Please upload an image or choose from the list:") 
+            elif args.lang == "ko": st.header("이미지를 업로드하거나 이미지를 선택해주세요:") 
+
+            input_points, input_labels = args.point_coords, args.point_labels    
+
+            get_im  = st.file_uploader('1', label_visibility='collapsed')
+            im_path = None
+            ims_lst, image_captions = get_ims_captions(path=args.root, n_ims=7)   
+
+            # Use the selected or uploaded image to get points and labels
+            if get_im: input_points, input_labels = get_clicked_point(get_im)          
                 
-                im_path = ims_lst[int(choice.split("#")[-1])-1]
-                input_points, input_labels = get_clicked_point(im_path)          
+            elif (get_im == None) and (im_path == None): 
                 
-    if (not input_points is None) and (not input_labels is None):
-        im_path = im_path if im_path != None else args.im_path
+                select_label = "Images List" if args.lang == "en" else "이미지 목록"
+                choice_label = "Available Images" if args.lang == "en" else "선택 가능한 이미지 목록"
+                placeholder  = "Please click to choose" if args.lang == "en" else "선택을 위해 클릭해주세요"
+                image_select(label=select_label, images=ims_lst, captions=image_captions)
+                choice = choose(option = image_captions, label = choice_label, placeholder = placeholder)
+                
+                if   args.lang == "en": st.header("Please upload an image or choose from the list!") 
+                elif args.lang == "ko": st.header("이미지를 선택하거나 업로드 해주세요!") 
+                
+                if choice:  
+                    
+                    im_path = ims_lst[int(choice.split("#")[-1])-1]
+                    input_points, input_labels = get_clicked_point(im_path)          
+                    
+        if (not input_points is None) and (not input_labels is None):
+            im_path = im_path if im_path != None else args.im_path
 
-        masks, inpaintings = inpaint(im_path, ckpts_path = args.ckpt_path, input_points = input_points, lama_config = args.lama_config, lama_ckpt = args.lama_ckpt,
-                input_labels = input_labels, device = args.device, output_dir = args.output_dir, dks = args.dilate_kernel_size, lang = args.lang)
+            masks, inpaintings = inpaint(im_path, ckpts_path = args.ckpt_path, input_points = input_points, lama_config = args.lama_config, lama_ckpt = args.lama_ckpt,
+                    input_labels = input_labels, device = args.device, output_dir = args.output_dir, dks = args.dilate_kernel_size, lang = args.lang)
 
-        cols = st.columns(len(masks))
+            cols = st.columns(len(masks))
 
-        for idx, col in enumerate(cols):
-            with col: write(f"Mask#{idx+1}:"); st.image(masks[idx])
+            for idx, col in enumerate(cols):
+                with col: write(f"Mask#{idx+1}:"); st.image(masks[idx])
 
-        cols = st.columns(len(masks))
+            cols = st.columns(len(masks))
 
-        for idx, col in enumerate(cols):
-            with col: write(f"Inpainting#{idx+1}:"); st.image(inpaintings[idx]) 
-        if   args.lang == "en": st.header("Please rerun streamlit script from the terminal!") 
-        elif args.lang == "ko": st.header("터미널에서 streamlit 스크립트를 다시 실행해 주세요!") 
+            for idx, col in enumerate(cols):
+                with col: write(f"Inpainting#{idx+1}:"); st.image(inpaintings[idx]) 
+            st.header(final_header) 
+
+    elif task_name in ["replace", "대체"]:
+        st.header("Hey there! I am replacer!")
 
 if __name__ == "__main__":
     
